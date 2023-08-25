420fdae4e69b2770a4a3f9411ee20196f73d1fc4
Leonardo Silva Dulci <leonardodulci@gmail.com>
2023-08-25 14:16:46 -0300

    .

--- script.js
+++ script.js
@@ -1,16 +1,4 @@
-                                                           
-    
-    var section = document.querySelector("#section")
-       
-    section.addEventListener("mouseover", function(){
-
-        var texto = document.querySelector(".text")
-
-        texto.classList.remove("text")
-    });
-
-
- 
+                                     
     
 
     
