c4be0307a1134e7833feb7bc540e45cf81deae63
Leonardo Silva Dulci <leonardodulci@gmail.com>
2023-09-08 16:14:32 -0300

    .

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-                                     
-    
+document.getElementById('section').addEventListener("mouseover", entrou);
+document.getElementById("section").addEventListener("mouseout", saiu);
 
-    
-
-
+function entrou() {
+    document.getElementById('section') = '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, porro quas magnam corporis culpa neque nesciunt sunt id, fugit voluptatum ab recusandae laborum sequi dolores illo harum officia qui quibusdam!</p>';
+   
+  }
+  
