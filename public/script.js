268cb7b0758fb1c3f1d5201e5fa9754510896273
Leonardo Silva Dulci <leonardodulci@gmail.com>
2023-10-17 11:09:27 -0300

    updates

---
+++ public/script.js
@@ -0,0 +1,21 @@
+// script.js
+document.addEventListener('DOMContentLoaded', function() {
+    const toggleButton = document.getElementById('toggle-button');
+    const text = document.getElementById('text');
+    const imagemLuana = document.getElementById('imagem-luana');
+
+    let isImageVisible = true;
+
+    toggleButton.addEventListener('click', function() {
+        if (isImageVisible) {
+            imagemLuana.style.display = 'none';
+            text.style.display = 'block';
+            toggleButton.innerHTML = '⬅'; // Mude a seta para a esquerda
+        } else {
+            imagemLuana.style.display = 'block';
+            text.style.display = 'none';
+            toggleButton.innerHTML = '➡'; // Mude a seta de volta para a direita
+        }
+        isImageVisible = !isImageVisible;
+    });
+});
