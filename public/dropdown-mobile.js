576e81d7e58eb86c3fbae9342f2d353c00c8b2b6
Leonardo Silva Dulci <leonardodulci@gmail.com>
2025-09-14 13:49:16 -0300

    update

---
+++ public/dropdown-mobile.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const dropBtn = document.querySelector(".dropbtn");
+  const dropdown = document.querySelector(".dropdown-content");
+
+  // Toggle ao clicar
+  dropBtn.addEventListener("click", function (e) {
+    e.preventDefault(); // previne o redirecionamento para #galeria
+    dropdown.classList.toggle("show-dropdown");
+  });
+
+  // Fecha se clicar fora
+  window.addEventListener("click", function (e) {
+    if (!e.target.closest(".nav-item")) {
+      dropdown.classList.remove("show-dropdown");
+    }
+  });
+});
